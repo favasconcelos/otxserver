c6a8921b60369d3c6c35e63aab54ee31c84f9153
granoob <granoob@gmail.com>
2018-07-18 12:28:14 -0300

    paralyze new formule 80%

--- data/spells/scripts/support/paralyze rune.lua
+++ data/spells/scripts/support/paralyze rune.lua
@@ -3,7 +3,7 @@ combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_RED)
 
 local condition = Condition(CONDITION_PARALYZE)
 condition:setParameter(CONDITION_PARAM_TICKS, 0)
-condition:setFormula(-0.9, 0, -0.9, 0)
+condition:setFormula(-1, 1, -1,1)
 combat:setCondition(condition)
 
 function onCastSpell(creature, var)
