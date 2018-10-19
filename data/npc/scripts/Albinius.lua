ceb08f52f2a97e22ae4ae810d7e2c393e6d2420f
Gom <gombk666@gmail.com>
2018-10-19 15:53:01 -0300

    General Fixes - NPC - Monsters - Map
    Fix stone Ankrahmun Position: 33236, 32577, 7
    UID In Service of Yalahar Position: 32822, 31162, 8
    
    Monsters:
    Annihilator: Angry Demon
    Bosses: Halls of Hope
    Sistema de morte para Thorn Knight e Time Guardian
    Bosses e mobs The First Dragon
    Falas NPC Albinius e Ivalisse

--- data/npc/scripts/Albinius.lua
+++ data/npc/scripts/Albinius.lua
@@ -15,7 +15,6 @@ local items = {
           item4 = {2667, 50735}, -- item1 e storage dado
           item5 = {21246, 50736}, -- item1 e storage dado
 		  item6 = {26654} -- item1 e storage dado
-		 
 }
 
 local counts = {
@@ -34,6 +33,7 @@ npcHandler:addModule(VoiceModule:new(voices))
 
 
 local function creatureSayCallback(cid, type, msg)
+	local player = Player(cid)
 	if not npcHandler:isFocused(cid) then
 		return false
 	end
@@ -45,23 +45,22 @@ local function creatureSayCallback(cid, type, msg)
 			}, cid)
 			end
 
-if msgcontains(msg, "tomes") then
-	  npcHandler:say("If you have some old shaper tomes I would {buy} them.", cid)
-end
-
+	if msgcontains(msg, "tomes") then
+		npcHandler:say("If you have some old shaper tomes I would {buy} them.", cid)
+	end
 
 	local talkUser = NPCHANDLER_CONVBEHAVIOR == CONVERSATION_DEFAULT and 0 or cid
-		local player = Player(cid)
+		
 
 		
 	    if msgcontains(msg, 'death portal') then
 		if player:getStorageValue(50730) == 1 then
 		if player:getStorageValue(items.item1[2]) <= 0 and getPlayerItemCount(cid, items.item1[1]) >= counts.count1[1] then
                               doPlayerRemoveItem(cid, items.item1[1], counts.count1[1])
-                              selfSay('Excellente! Now you can pass in the teleport.', cid)
+                              selfSay('Excellent! Now you can pass in the teleport.', cid)
 							 player:setStorageValue(items.item1[2], counts.count1[2])
                     else
-                              selfSay('You need '.. counts.count1[1] ..' '.. getItemName(items.item1[1]) ..'.', cid)
+                              selfSay('You may pass this portal if you have 50 pelvis bones as offering. Do you have the bones with you?', cid)
         end
 		else
                               selfSay('Sorry, first you need to bring my Heavy Old Tomes.', cid)
@@ -71,10 +70,10 @@ end
 					if player:getStorageValue(50730) == 1 then
 		  if player:getStorageValue(items.item2[2]) <= 0 and getPlayerItemCount(cid, items.item2[1]) >= counts.count1[1] then
                               doPlayerRemoveItem(cid, items.item2[1], counts.count1[1])
-                              selfSay('Excellente! Now you can pass in the teleport.', cid)
+                              selfSay('Excellent! Now you can pass in the teleport.', cid)
 							 player:setStorageValue(items.item2[2], counts.count1[2])
                     else
-                              selfSay('You need '.. counts.count1[1] ..' '.. getItemName(items.item2[1]) ..'.', cid)
+                              selfSay('You may pass this portal if you have 50 marsh stalker feathers as offering. Do you have the feathers with you?', cid)
                     end
 		else
                               selfSay('Sorry, first you need to bring my Heavy Old Tomes.', cid)
@@ -82,7 +81,7 @@ end
 		elseif msgcontains(msg, 'temple') then
 		  if player:getStorageValue(50730) <= 0 and  player:getItemCount(26654) > 4 then
 							  doPlayerRemoveItem(cid, 26654, 5)
-                              selfSay('Excellente! Now you can use the imbuing shrines.', cid)
+                              selfSay('Thank you very much for your contribution, child. Your first step in the ways of the Shapers has been taken.', cid)
 							 player:setStorageValue(50730, 1)
           else
                               selfSay('You need 5 Heavy Old Tomes', cid)
@@ -92,22 +91,35 @@ end
 					if player:getStorageValue(50730) == 1 then
 		  if player:getStorageValue(items.item3[2]) <= 0 and getPlayerItemCount(cid, items.item3[1]) >= counts.count1[1] then
                               doPlayerRemoveItem(cid, items.item3[1], counts.count1[1])
-                              selfSay('Excellente! Now you can pass in the teleport.', cid)
+                              selfSay('Excellent! Now you can pass in the teleport.', cid)
 							 player:setStorageValue(items.item3[2], counts.count1[2])
                     else
-                              selfSay('You need '.. counts.count1[1] ..' '.. getItemName(items.item3[1]) ..'.', cid)
+                              selfSay('You may pass this portal if you have 50 acorns as offering. Do you have the acorns with you?', cid)
                     end
 		else
                               selfSay('Sorry, first you need to bring my Heavy Old Tomes.', cid)
 		end
+		elseif msgcontains(msg, 'last portal') then
+			if player:getStorageValue(50730) == 1 then
+				if player:getStorageValue(Storage.BossHoH.Progresso) >= 7 then
+					selfSay('Be careful child, great dangers are ahead of you.', cid)
+					player:setStorageValue(50737, 1)
+				elseif player:getStorageValue(50737) >= 1 then
+					selfSay('You can already visit the new shapers ruins. Be careful.', cid) 
+				else
+					selfSay("You don't have enough knowledge to enter the dimensional portal.", cid)
+				end
+			else
+					selfSay('Sorry, first you need to bring my Heavy Old Tomes.', cid)
+			end
 					elseif msgcontains(msg, 'ice portal') then
 		if player:getStorageValue(50730) == 1 then
 		  if player:getStorageValue(items.item4[2]) <= 0 and getPlayerItemCount(cid, items.item4[1]) >= counts.count1[1] then
                               doPlayerRemoveItem(cid, items.item4[1], counts.count1[1])
-                              selfSay('Excellente! Now you can pass in the teleport.', cid)
-							 player:setStorageValue(items.item4[2], counts.count1[2])
+                              selfSay('Excellent! Now you can pass in the teleport.', cid)
+							  player:setStorageValue(50735, 1)
                     else
-                              selfSay('You need '.. counts.count1[1] ..' '.. getItemName(items.item4[1]) ..'.', cid)
+                              selfSay('You may pass this portal if you have 50 fishes as offering. Do you have the fishes with you?', cid)
                     end
 		else
                               selfSay('Sorry, first you need to bring my Heavy Old Tomes.', cid)
@@ -116,14 +128,14 @@ end
 					if player:getStorageValue(50730) == 1 then
 		  if player:getStorageValue(items.item5[2]) <= 0 and getPlayerItemCount(cid, items.item5[1]) >= counts.count1[1] then
                               doPlayerRemoveItem(cid, items.item5[1], counts.count1[1])
-                              selfSay('Excellente! Now you can pass in the teleport.', cid)
+                              selfSay('Excellent! Now you can pass in the teleport.', cid)
 							 player:setStorageValue(items.item5[2], counts.count1[2])
                     else
-                              selfSay('You need '.. counts.count1[1] ..' '.. getItemName(items.item5[1]) ..'.', cid)
+                              selfSay('You may pass this portal if you have 50 incantation notes as offering. Do you have the notes with you?', cid)
                     end
 		else
                               selfSay('Sorry, first you need to bring my Heavy Old Tomes.', cid)
-		end			
+		end
 					
 end
           return TRUE
@@ -131,6 +143,9 @@ end
 
 
 keywordHandler:addKeyword({'job'}, StdModule.say, {npcHandler = npcHandler, text = "I find ways to unveil the secrets of the stars. Judging by this question, I doubt you follow my weekly publications concerning this research."})
+keywordHandler:addKeyword({'name'}, StdModule.say, {npcHandler = npcHandler, text = "I am Albinius, a worshipper of the {Astral Shapers}."})
+
+
 npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
 npcHandler:setMessage(MESSAGE_GREET, "Greetings, pilgrim. Welcome to the halls of hope. We are the keepers of this {temple} and welcome everyone willing to contribute.")
 npcHandler:setMessage(MESSAGE_FAREWELL, "Farewell, my child")
