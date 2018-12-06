7e0006f74c5bbe8d1fac0c737d746f3af895d4d8
Felipe Vasconcelos <fav@cesar.org.br>
2018-12-06 00:35:29 -0300

    Fixing message encoding

--- data/globalevents/scripts/autobroadcast.lua
+++ data/globalevents/scripts/autobroadcast.lua
@@ -4,10 +4,10 @@ function onThink(interval, lastExecution)
   local messages = {
     "[TICKETS]: Utilize nosso atendimento por tickets para falar com um membro superior.",
     "[TEAMSPEAK3]: Servidor de teamSpeak 3 com 1000 slots disponiveis, liberado para criar salas particulares e com senha.",
-    "[REGRAS]: Evite ser punido, n�o viole nossas regras.",
-    "[SISTEMA]: O global save do servidor ocorre todos os dias �s 06:00hrs.",
-    "[SEGURAN�A]: Nunca use a mesma senha de outros servidores, pois voc� estar� facilitando a vida dos hackers.",
-    "[SEGURAN�A]: Proteja sua senha. Use-a apenas em nosso website oficial."
+    "[REGRAS]: Evite ser punido, não viole nossas regras.",
+    "[SISTEMA]: O global save do servidor ocorre todos os dias às 06:00hrs.",
+    "[SEGURANÇA]: Nunca use a mesma senha de outros servidores, pois você estará facilitando a vida dos hackers.",
+    "[SEGURANÇA]: Proteja sua senha. Use-a apenas em nosso website oficial."
   }
 
   Game.broadcastMessage(messages[math.random(#messages)], MESSAGE_EVENT_ADVANCE)
