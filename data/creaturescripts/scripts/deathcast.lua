3b11a3b85eee21b2f67121418c66c000d793e0d2
Guilherme Maneskul <guilherme@maneskul.com.br>
2018-12-06 10:21:49 -0200

    Removing unnecessary files in talkactions, creaturescripts, globalevents and movements

--- data/creaturescripts/scripts/deathcast.lua
+++
@@ -1,9 +0,0 @@
-function onKill(cid, target, lastHit)
-  if isPlayer(target) == true then
-    Game.broadcastMessage(
-      "[" .. getCreatureName(cid) .. "] acabou de matar o [" .. getCreatureName(target) .. "]",
-      MESSAGE_EVENT_ADVANCE
-    )
-  end
-  return true
-end
--- data/globalevents/scripts/castleEvent/tempo.lua
+++
@@ -1,8 +0,0 @@
-dofile("data/lib/events/castelo.lua")
-function onTime()
-  local day = os.date("%A")
-  if (day == DAY) then
-    startCastleEvent()
-  end
-  return true
-end
