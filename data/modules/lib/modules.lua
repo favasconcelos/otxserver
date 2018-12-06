848510e3186cd8e8e03bba67d129f3e68ad6812a
Felipe Vasconcelos <fav@cesar.org.br>
2018-12-06 10:22:21 -0300

    Updating file encoding

--- data/modules/lib/modules.lua
+++ data/modules/lib/modules.lua
@@ -17,18 +17,3 @@ function addPlayerEvent(callable, delay, playerId, ...)
     ...
   )
 end
-
---[[
-function Player.updateFightModes(self)
-	local msg = NetworkMessage()
-
-	msg:addByte(0xA7)
-
-	msg:addByte(self:getFightMode())
-	msg:addByte(self:getChaseMode())
-	msg:addByte(self:getSecureMode() and 1 or 0)
-	msg:addByte(self:getPvpMode())
-
-	msg:sendToPlayer(self)
-end
-]]
