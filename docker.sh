aca4eaee465e146517e1faab474af1d36f36ffec
Felipe Vasconcelos <fav@cesar.org.br>
2019-01-28 18:35:52 -0300

    Adding script to unzip map before runnig docker and adding the map to the gitignore

--- docker.sh
+++ docker.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+
+if ! [ -x "$(command -v unzip)" ]; then
+  echo 'Error: unzip is not installed.' >&2
+  exit 1
+fi
+
+# Config
 MAP_PATH="./data/world/"
 MAP_FILE="realmap.otbm"
-# check if the map does not exists
+# check if the map does not exists and unzip it
 if [ ! -f "$MAP_PATH$MAP_FILE" ]; then
   unzip "$MAP_PATH$MAP_FILE.zip" -d $MAP_PATH
 fi
-# docker-compose -p otxserver -f docker-compose.yaml up -d
+docker-compose -p otxserver -f docker-compose.yaml up -d
